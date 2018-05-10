086f2a2b12d172fb94a5e09a3ef54f57c2ef70bf
florian <florian.meyer86@gmail.com>
2018-05-10 15:25:04 +0200

    asd

--- webServer/server/index.js
+++ webServer/server/index.js
@@ -1,5 +1,5 @@
 import { port } from '../config/config';
 import Server   from './server';
 
-console.log('asdssssssssAAAaASDsssssassaswwss12');
+console.log('asdssssssssAAAaASDsssssassaswwssss12');
 new Server(port);
