cee9fd747f97afc87d1006f70479c9656bd56b3d
FL0RIANMEYER <florian.meyer86@gmail.com>
2018-06-06 08:58:11 +0200

    "Change #57: webServer/server/server.js"

---
+++ webServer/server/server.js
@@ -0,0 +1,18 @@
+import express    from 'express';
+import bodyParser from 'body-parser';
+import routes         from './routes';
+
+
+const Server = function(port) {
+    const app = express();
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(express.static(__dirname + '/public', {
+        index: false,
+    }));
+    app.use(routes);
+
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+};
+
+export default Server;
