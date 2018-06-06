ca520862c73aa4e5a6e36ff5b14189ac81aa9945
FL0RIANMEYER <florian.meyer86@gmail.com>
2018-06-06 09:19:39 +0200

    "Change #97: webServer/config/webpack/server/webpack.prod.js"

--- webServer/config/webpack/server/webpack.prod.js
+++ webServer/config/webpack/server/webpack.prod.js
@@ -22,7 +22,7 @@ export default {
     },
     module: {
         rules: [{
-                test: /\.js$/,
+                test: /\.jsx$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
 
