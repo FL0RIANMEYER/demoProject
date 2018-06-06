82c1b9587d082fe19ef071630bfc9ea99ace27dc
FL0RIANMEYER <florian.meyer86@gmail.com>
2018-06-06 08:41:38 +0200

    "Change #34: webServer/app/index.js"

--- webServer/app/index.js
+++ webServer/app/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './components/app';
+import store from './store';
 
 import './index.css';
 
 
-hydrate(<App/>, document.getElementById('root'));
+hydrate(<Provider store={store}>
+    <App/>
+</Provider>, document.getElementById('root'));
