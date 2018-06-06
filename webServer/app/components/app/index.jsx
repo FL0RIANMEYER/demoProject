8d2e8588a0dd2926a5a3f2d967cba626c5936fdf
FL0RIANMEYER <florian.meyer86@gmail.com>
2018-06-06 09:07:41 +0200

    "Change #73: webServer/app/components/app/index.jsx"

--- webServer/app/components/app/index.jsx
+++ webServer/app/components/app/index.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 import headerContainer from '../../container/header';
 import headerComponent from '../../components/header';
+import mainSectionContainer from '../../container/mainsection';
+import mainSectionComponent from '../../components/mainsection';
 
 const Header = headerContainer(headerComponent);
+const MainSection = mainSectionContainer(mainSectionComponent);
 
 import styles from './index.css';
 
 const App = () => (
     <section className={styles.todoapp}>
         <Header />
+        <MainSection />
     </section>
 );
 
