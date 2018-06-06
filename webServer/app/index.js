7845e0e04c51dae1d0b981bd74121f2f5e615d92
FL0RIANMEYER <florian.meyer86@gmail.com>
2018-06-06 09:24:42 +0200

    "Change #99: webServer/app/index.js"

--- webServer/app/index.js
+++ webServer/app/index.js
@@ -4,9 +4,11 @@ import { Provider } from 'react-redux';
 
 import App from './components/app';
 import store from './store';
+import { getTodos } from './actions';
 
 import './index.css';
 
+store.dispatch(getTodos());
 
 hydrate(<Provider store={store}>
     <App/>
