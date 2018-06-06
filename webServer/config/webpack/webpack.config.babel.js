2fcccf254fe2e68860140c2eb8585301b5231ea8
FL0RIANMEYER <florian.meyer86@gmail.com>
2018-06-06 12:35:20 +0200

    "Change #9: webServer/config/webpack/webpack.config.babel.js"

---
+++ webServer/config/webpack/webpack.config.babel.js
@@ -0,0 +1,13 @@
+import general from './webpack.general.babel.js';
+import client  from './webpack.client.babel.js';
+import server  from './webpack.server.babel.js';
+
+export default (env, args) => {
+    const config = [
+        client( env, args),
+        server( env, args),
+        general(env, args),
+    ];
+
+    return config.filter(c => !!c);
+}; 
