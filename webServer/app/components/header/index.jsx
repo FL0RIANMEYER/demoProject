40b08fe4b39b673003a1fe84cb2fefe495d53beb
FL0RIANMEYER <florian.meyer86@gmail.com>
2018-06-06 10:01:10 +0200

    "Change #110: webServer/app/components/header/index.jsx"

--- webServer/app/components/header/index.jsx
+++ webServer/app/components/header/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TodoTextInput from '../../components/todo/TodoTextInput';
 
 import styles from './index.css';
 
@@ -7,11 +8,20 @@ import styles from './index.css';
 export const Header = ({ addTodo }) => (
   <header className={styles.header}>
     <h1>todos</h1>
+    <TodoTextInput
+      newTodo
+      onSave={(text) => {
+        if (text.length !== 0) {
+          addTodo(text);
+        }
+      }}
+      placeholder="What needs to be done?"
+    />
   </header>
 );
 
 Header.propTypes = {
   addTodo: PropTypes.func.isRequired,
-}; 
+};
 
 export default Header;
